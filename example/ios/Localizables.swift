12a66adbac2680bc80e1ba6ec34114c67427e6ed
prongbang <prongbang@gmail.com>
2021-09-18 16:14:09 +0700

    Update example

--- example/ios/Localizables.swift
+++ example/ios/Localizables.swift
@@ -1,10 +1,11 @@
 import Foundation
 
-public enum TranslateType: String {
+public enum LocalizablesType: String {
 	case app_name
 	case common_ok
 	case common_no
 	case common_yes
 	case common_cancel
 	case login_entered_password_wrong
+	case status_code
 }
