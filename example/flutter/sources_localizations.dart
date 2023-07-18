56e651c41553ad59487e1d0695d342394861289c
prongbang <prongbang@gmail.com>
2023-07-18 13:49:51 +0700

    Update localizegen

--- example/flutter/sources_localizations.dart
+++ example/flutter/sources_localizations.dart
@@ -20,9 +20,9 @@ class SourcesLocalizations {
 		'app_name': 'localizgen',
 		'common_ok': 'OK',
 		'common_no': 'NEIN',
-		'common_yes': 'JAWOHL',
-		'common_cancel': 'ABBRECHEN',
-		'common_login_entered_password_wrong': 'Sie haben falsche Kennwort %s-mal eingegeben, verbleibende %s -ZEZE.',
+		'common_yes': 'JA',
+		'common_cancel': 'STORNIEREN',
+		'common_login_entered_password_wrong': 'Sie haben ein falsches Passwort %s Zeiten eingegeben und %s Zeiten verbleiben.',
 		'common_status_code': 'Status Code: %s',
 		'common_error_code': 'Error Code: %s',
 	};
@@ -40,18 +40,18 @@ class SourcesLocalizations {
 
 	Map<String, String> get _es => {
 		'app_name': 'localizgen',
-		'common_ok': 'OK',
+		'common_ok': 'DE ACUERDO',
 		'common_no': 'NO',
 		'common_yes': 'SÍ',
 		'common_cancel': 'CANCELAR',
-		'common_login_entered_password_wrong': 'Ingresó una contraseña incorrecta %s veces, restante %s veces.',
+		'common_login_entered_password_wrong': 'Ingresó una contraseña incorrecta %s veces, restantes %s veces.',
 		'common_status_code': 'Status Code: %s',
 		'common_error_code': 'Error Code: %s',
 	};
 
 	Map<String, String> get _fr => {
 		'app_name': 'localizgen',
-		'common_ok': 'd\'accord',
+		'common_ok': 'D\'ACCORD',
 		'common_no': 'NON',
 		'common_yes': 'OUI',
 		'common_cancel': 'ANNULER',
@@ -63,8 +63,8 @@ class SourcesLocalizations {
 	Map<String, String> get _hi => {
 		'app_name': 'localizgen',
 		'common_ok': 'ठीक',
-		'common_no': 'ना',
-		'common_yes': 'हां',
+		'common_no': 'नहीं',
+		'common_yes': 'हाँ',
 		'common_cancel': 'रद्द करना',
 		'common_login_entered_password_wrong': 'आपने गलत पासवर्ड %s बार, शेष %s बार दर्ज किया।',
 		'common_status_code': 'Status Code: %s',
@@ -73,11 +73,11 @@ class SourcesLocalizations {
 
 	Map<String, String> get _ja => {
 		'app_name': 'localizgen',
-		'common_ok': 'わかった',
+		'common_ok': 'OK',
 		'common_no': 'いいえ',
 		'common_yes': 'はい',
 		'common_cancel': 'キャンセル',
-		'common_login_entered_password_wrong': '誤ったパスワード%s回、残りの%s回。',
+		'common_login_entered_password_wrong': '間違ったパスワードを入力し、%sを残り、%sを残しました。',
 		'common_status_code': 'Status Code: %s',
 		'common_error_code': 'Error Code: %s',
 	};
@@ -88,18 +88,18 @@ class SourcesLocalizations {
 		'common_no': 'NÃO',
 		'common_yes': 'SIM',
 		'common_cancel': 'CANCELAR',
-		'common_login_entered_password_wrong': 'Você digitou senha errada %s vezes, restante %s vezes.',
+		'common_login_entered_password_wrong': 'Você inseriu a senha errada %s vezes, permanecendo %s vezes.',
 		'common_status_code': 'Status Code: %s',
 		'common_error_code': 'Error Code: %s',
 	};
 
 	Map<String, String> get _ru => {
 		'app_name': 'localizegen',
-		'common_ok': 'Ok',
+		'common_ok': 'ХОРОШО',
 		'common_no': 'НЕТ',
 		'common_yes': 'ДА',
-		'common_cancel': 'ОТМЕНИТЬ',
-		'common_login_entered_password_wrong': 'Вы ввели неправильный пароль %s раз, оставаясь %s раз.',
+		'common_cancel': 'ОТМЕНА',
+		'common_login_entered_password_wrong': 'Вы ввели неправильный пароль %s раз, оставшиеся %s раз.',
 		'common_status_code': 'Status Code: %s',
 		'common_error_code': 'Error Code: %s',
 	};
@@ -121,7 +121,7 @@ class SourcesLocalizations {
 		'common_no': '不',
 		'common_yes': '是的',
 		'common_cancel': '取消',
-		'common_login_entered_password_wrong': '您输入了错误的密码%s时间，剩下%s时间。',
+		'common_login_entered_password_wrong': '您输入了错误的密码%s次，保留%s次。',
 		'common_status_code': 'Status Code: %s',
 		'common_error_code': 'Error Code: %s',
 	};
